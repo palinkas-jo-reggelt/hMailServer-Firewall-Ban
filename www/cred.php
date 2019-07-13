83699c92b70519bc8c860d37b620e43f13cb99f3
palinkas-jo-reggelt <52140617+palinkas-jo-reggelt@users.noreply.github.com>
2019-07-12 20:45:39 -0400

    Update cred.php

--- www/cred.php
+++ www/cred.php
@@ -2,7 +2,7 @@
 // Fill in variables
 $m_host="localhost";
 $m_dbuser="hmailserver";
-$m_dbpass="SSnGLBs8XswL2r0h";
+$m_dbpass="supersecretpassword";
 $m_db="hmailserver";
 
 	$con=mysqli_connect($m_host,$m_dbuser,$m_dbpass,$m_db);
