17211a6862efb76255f493edc6893bcbcd03dc63
Ad Min <admin@123.dynu.com>
2019-11-12 21:33:18 -0500

    v.0.50
    0.50 simplified BlockCount.ps1; automated end of script

--- BlockCount.ps1
+++ BlockCount.ps1
@@ -17,6 +17,7 @@ ____ _ ____ ____ _ _ _  __  _    _       ___   __  _  _
 	Run whenever you're curious
 
 .NOTES
+	Script runs until there are 0 firewall drops for a given number of days
 	
 .EXAMPLE
 
@@ -31,7 +32,7 @@ $MySQLHost          = 'localhost'                                          #
 #                                                                          #
 ############################################################################
 
-Function MySQLQuery($Query) {
+Function MySQLQuery ($Query) {
 	$ConnectionString = "server=" + $MySQLHost + ";port=3306;uid=" + $MySQLAdminUserName + ";pwd=" + $MySQLAdminPassword + ";database=" + $MySQLDatabase
 	Try {
 	  $Today = (Get-Date).ToString("yyyyMMdd")
@@ -98,272 +99,19 @@ $NeverBlocked = ([int]$TotalRules - [int]$TotalReturnIPs)
 $PercentNever = ([int]$NeverBlocked / [int]$TotalRules).ToString("P")
 Write-Output ("{0,7} : {1,6} : Number of return IPs never blocked" -f ($NeverBlocked).ToString("#,###"), $PercentNever)
 Write-Output " "
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 1 day" -f ($TotalReturnIPs).ToString("#,###"), $PercentReturns)
 
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 1"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$NumHits = $Rows.totalhits
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 2 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
+$a = 0
+$b = 1
 
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 2"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 3 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 3"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 4 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 4"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 5 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 5"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 6 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 6"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 7 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 7"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 8 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 8"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 9 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 9"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 10 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 10"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 11 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 11"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 12 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 12"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 13 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 13"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 14 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 14"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 15 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 15"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 16 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 16"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 17 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 17"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 18 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 18"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 19 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 19"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 20 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 20"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 21 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 21"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 22 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 22"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 23 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 23"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 24 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 24"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 25 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 25"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 26 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 26"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 27 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 27"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 28 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 28"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 29 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 29"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 30 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 30"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 31 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 31"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 32 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 32"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 33 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 33"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 34 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 34"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 35 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 35"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 36 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 36"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 37 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 37"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 38 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 38"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 39 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 39"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 40 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 40"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 41 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 41"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 42 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 42"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 43 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 43"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 44 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
-
-$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > 44"
-$Rows = @(MySQLQuery($Query))
-$ReturnIPs = $Rows.Rows.Count
-$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
-Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least 45 days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
+Do {
+	$Query = "SELECT ipaddress, COUNT(DISTINCT(DATE(timestamp))) AS countdate FROM hm_fwban_rh GROUP BY ipaddress HAVING countdate > $a"
+	$Rows = @(MySQLQuery($Query))
+	$ReturnIPs = $Rows.Rows.Count
+	$PercentReturns = ($ReturnIPs / $TotalRules).ToString("P")
+	Write-Output ("{0,7} : {1,6} : Number of return IPs blocked on at least $b days" -f ($ReturnIPs).ToString("#,###"), $PercentReturns)
+	$a++
+	$b++
+} Until ($ReturnIPs -lt 1)
 
 Write-Output " "
 Write-Output " "
